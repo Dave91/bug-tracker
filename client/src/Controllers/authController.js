a5fc1e7df5b09e56bd9c4b1f1957d54b82e85905
Dave91 <thecothineal@gmail.com>
2023-02-15 14:16:50 +0100

    *

--- client/src/Controllers/authController.js
+++ client/src/Controllers/authController.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
-//import { Firestore } from "firebase/firestore";
+import fb from "../config/fbconfig";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -9,15 +9,7 @@ import {
   updateCurrentUser,
 } from "firebase/auth";
 
-const app = initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  //databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-});
+const app = initializeApp(fb);
 
 //export const db = Firestore();
 
